25b9c56ee02ad07184b8488f080fe16ff1299dfe
Valentina Valverde <vvgreenville@gmail.com>
2024-01-10 14:35:34 -0500

    Completed assignment #3

---
+++ src/components/ExerciseThree.tsx
@@ -0,0 +1,27 @@
+interface Props {
+  className: string;
+}
+
+// /* @ts-expect-error */
+export const Button: React.FC<Props> = (props) => {
+  return {
+    ohDear: "123",
+  };
+};
+
+const Parent = () => {
+  return (
+    <>
+      <Button className="my-class"></Button>
+    </>
+  );
+};
+
+export default Parent;
+
+// the assignment here was to find a way for the error to show up sooner
+// in the file instead of at line 15, where stuff is actually returned
+
+// the solution to this problem was to add React.FC on line 6
+// you can also pass in props through .FC (as shown above)
+// props can no longer be passed through (props: Props)
