8c9faa55ce57b48bddd8f9db9fe48a795c243d46
Valentina Valverde <vvgreenville@gmail.com>
2024-01-11 15:45:50 -0500

    Finished assignment #5

---
+++ src/hooks_exercises/AssignmentFive.tsx
@@ -0,0 +1,25 @@
+import { useCallback } from "react";
+
+export const Buttons = (props: { id: string }) => {
+  const onClick = useCallback(
+    (buttonName: string) => {
+      console.log(props.id, buttonName);
+    },
+    [props.id]
+  );
+
+  type test = Expect<Equal<typeof onClick, (buttonName: string) => void>>;
+
+  return (
+    <div>
+      <button onClick={() => onClick("A")}>A</button>
+      <button onClick={() => onClick("B")}>B</button>
+      <button onClick={() => onClick("C")}>C</button>
+    </div>
+  );
+};
+
+// instead of putting the string type on useCallback like so:
+// useCallback<string>
+
+//put it directly on buttonName instead (line 5)
