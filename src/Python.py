902f8376b15ded8d3993e51482271601b08acce1
Valentina Valverde <vvgreenville@gmail.com>
2024-01-12 15:59:39 -0500

    Fixed python syntax errors

--- src/Python.py
+++ src/Python.py
@@ -1,9 +1,9 @@
-# hai = input("how was your day?")
+hai = input("how was your day?")
 
 
-# if hai == ("bad")(
-#     print ("oh noo whyyy??")
-# ) else (
-#     print ("meow")
-# )
+if hai == "bad":
+    print ("oh noo whyyy??")
+else:
+    print ("meow")
+
 
